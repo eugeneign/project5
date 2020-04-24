3648c359aa04fa099cc8b221951542b838cf57a7
eugeneign <49946057+eugeneign@users.noreply.github.com>
2020-04-24 13:08:53 +0300

    24.04

---
+++ webpack.config.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  context: path.resolve(__dirname, './'),
+  entry: ['./src/index.js', './src/styles.scss'],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    stats: 'errors-only',
+    hot: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: require.resolve('jquery'),
+        use: [
+          { loader: 'expose-loader', options: 'jQuery' },
+          { loader: 'expose-loader', options: '$' }
+        ]
+      },
+      
+      {
+        test: require.resolve('tether'),
+        use: [
+          { loader: 'expose-loader', options: 'Tether' }
+        ]
+      },
+      {
+        test: /bootstrap\/dist\/js\/umd\//, use: 'imports-loader?jQuery=jquery'
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: isDev
+          ? ['style-loader', 'css-loader', 'sass-loader']
+          : [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: {
+                  safe: true
+                }
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                autoprefixer: {
+                  browsers: ['last 2 versions']
+                },
+                plugins: () => require('autoprefixer')
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {}
+            }
+          ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
